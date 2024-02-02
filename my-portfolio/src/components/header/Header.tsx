683b7b20fb0cfbd0fb1d78295be94d1a78838492
ariashector19 <ariashector19@gmail.com>
2024-02-02 12:45:32 +0100

    Adding some styles

--- my-portfolio/src/components/header/Header.tsx
+++ my-portfolio/src/components/header/Header.tsx
@@ -1,25 +1,40 @@
-import { Link } from "react-router-dom";
-import { ABOUT_ME, CONTACT, PROJECTS, SKILLS } from "../../utils/Props";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+import "./header.scss";
 
-export const Header = () => {
+export const Header: React.FC = () => {
+  const { pathname } = useLocation();
+
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to={ABOUT_ME}>About Me</Link>
-          </li>
-          <li>
-            <Link to={SKILLS}>Skills</Link>
-          </li>
-          <li>
-            <Link to={PROJECTS}>Projects</Link>
-          </li>
-          <li>
-            <Link to={CONTACT}>Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <motion.header initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <header>
+        <div className="header-text">Mi Potfolio</div>
+        <nav>
+          <ul className="horizontal-list">
+            <li className={pathname.includes("/aboutme") ? "selected" : ""}>
+              <Link to="/aboutme" className="accent">
+                About Me
+              </Link>
+            </li>
+            <li className={pathname.includes("/skills") ? "selected" : ""}>
+              <Link to="/skills" className="accent">
+                Skills
+              </Link>
+            </li>
+            <li className={pathname.includes("/projects") ? "selected" : ""}>
+              <Link to="/projects" className="accent">
+                Projects
+              </Link>
+            </li>
+            <li className={pathname.includes("/contact") ? "selected" : ""}>
+              <Link to="/contact" className="accent">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </motion.header>
   );
 };
